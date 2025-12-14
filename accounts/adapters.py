ed414ab5b9818381b3214ab3c7d0dc068c0927a3
Lucifer <lucifer911m@gmail.com>
2025-12-14 17:29:40 +0500

    Fix allauth AccountAdapter to send verification emails

--- accounts/adapters.py
+++ accounts/adapters.py
@@ -1,6 +1,11 @@
 from allauth.account.adapter import DefaultAccountAdapter
 from django.urls import reverse
 
+
 class AccountAdapter(DefaultAccountAdapter):
     def get_login_redirect_url(self, request):
         return reverse("dashboard")
+
+    def send_confirmation_mail(self, request, emailconfirmation, signup):
+        # VERY IMPORTANT: call parent method so email is actually sent
+        super().send_confirmation_mail(request, emailconfirmation, signup)
